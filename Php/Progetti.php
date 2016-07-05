ae4cd9fd08e848dba10089bd479a3eb32276f330
Z3rbi99 <iacopo.zerbetto@gmail.com>
2016-07-05 14:43:07 +0200

    prog

--- Php/Progetti.php
+++ Php/Progetti.php
@@ -7,42 +7,37 @@ if(!$conn) {
 } else {
     echo 'Connessione riuscita !<br />';
 }
-?>
-<form action="Persone.php">
-	<button type=”button”>
-		Mostra progetti
-	</button>
-<?php
-if(!$queryprogetti = @pg_query("select * from PROGETTI"))
-	die("Errore nella query: " . pg_last_error($conn));
 
-echo <<<EOD
-<table border="1" cellspacing="2" cellpadding="2">
-    <tr>
-        <td><b>Id</b></td>
-		<td><b>Nome</b></td>
-        <td><b>Descrizione</b></td>
-		<td><b>Data Inizio</b></td>
-		<td><b>Data Fine</b></td>
-    </tr>
+
+
+function showTab(){
+	if(!$queryprogetti = @pg_query("select * from PROGETTI"))
+		die("Errore nella query: " . pg_last_error($conn));
+
+	echo <<<EOD
+		<table border="1" cellspacing="2" cellpadding="2">
+			<tr>
+				<td><b>Id</b></td>
+				<td><b>Nome</b></td>
+				<td><b>Descrizione</b></td>
+				<td><b>Data Inizio</b></td>
+				<td><b>Data Fine</b></td>
+			</tr>
 EOD;
 
+	while($prog = pg_fetch_assoc($queryprogetti)){
+		echo "\n\t<tr>\n\t\t<td>".$prog['id']."</td>\n\t\t<td>".$prog['nome']."</td>";
+		echo "<td>".$prog['descrizione']."</td>\n\t\t<td>".$prog['datainizio']."</td>\n\t<td>".$prog['datafine']."</td>\n\t</tr>";
+	}	
+}
+?>
 
-
-
-while($prog = pg_fetch_assoc($queryprogetti)){
-		echo 
-		}	
+<form action="Persone.php">
+	<input type="submit" value="Mostra progetti">
 	
-?>	
-	
-	
-
+<?php  showTab() ?>	
 	
 </form>
-
-
-
 
 
 
